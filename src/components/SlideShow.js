20369cbda1db2a931ec2b736a5cca1b1311ebfb9
Mathieu Poclet <poclet.mathieu@outlook.fr>
2024-05-12 20:05:44 +0200

    slideShow fini

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,69 +1,46 @@
 import "../styles/slideShow.scss"
 import { useState } from "react";
+//import {FontAwesomeIcon} from "react"
 
 function SlideShow({datas, setDatas}) {
 
     const [index, setIndex] = useState(0)
 
-    const handelClick = () => {  
-        setIndex(index + 1 )         
-      }    
-    
+    const previous = (data) => {
+        index === 0 ? setIndex(data.pictures.length - 1) : setIndex(index - 1)
+    }    
+
+    const next = (data) => {
+        index === data.pictures.length - 1 ? setIndex(0) : setIndex(index + 1)
+    }    
+        
     return (
         <div>            
-            {datas.map((data) => {           
-                return (                   
-                    <div key={`${data.title}-${index}`} className="gallery">
-                        <img src={data.pictures[index]} alt="vue appartement" />
-                        <button onClick={handelClick}>suivant</button>
-                        <h2>{index}</h2>
-                    </div>
-                )
+            {datas.map((data) => {  
+                if(data.pictures.length > 1) {
+                    return (                   
+                        <div key={`${data.title}-${index}`} className="gallery">
+                            <img src={data.pictures[index]} alt="vue appartement" />                    
+                            <button className="left" onClick={() => previous(data)}><i className="fa-solid fa-chevron-left pointer"></i></button>
+                            <button className="right" onClick={() => next(data)}><i className="fa-solid fa-chevron-right pointer"></i></button>                    
+                            <div className="count">{index + 1}/{data.pictures.length}</div>
+                        </div>
+                    )
+                } else {
+                    return (
+                        <div className="gallery">
+                            <img src={data.pictures[index]} alt="vue appartement" /> 
+                        </div>
+                    )
+                }     
+                
             })}                
         </div>
     )
 }
 
+
 export default SlideShow
 
-// function SlideShow({ datas }) {
-//     const [currentSlide, setCurrentSlide] = useState(0); // State to track current slide
 
-//     const nextSlide = () => {
-//         setCurrentSlide((prevSlide) => (prevSlide + 1) % datas.length);
-//     };
-
-//     const prevSlide = () => {
-//         setCurrentSlide((prevSlide) => (prevSlide - 1 + datas.length) % datas.length);
-//     };
-
-//     return (
-//         <div className="slideshow-container">
-//             <div className="slides">
-//                 {datas.map((data, index) => (
-//                     <div key={data.id} className={index === currentSlide ? "slide active" : "slide"}>
-//                         <img src={data.pictures[0]} alt={data.title} />
-//                     </div>
-//                 ))}
-//             </div>
-//             {datas.length > 1 && (
-//                 <div className="controls">
-//                     <button onClick={prevSlide}>&#10094;</button>
-//                     <button onClick={nextSlide}>&#10095;</button>
-//                 </div>
-//             )}
-//             {datas.length > 1 && (
-//                 <div className="pagination">
-//                     {datas.map((data, index) => (
-//                         <span
-//                             key={data.id}
-//                             className={index === currentSlide ? "dot active" : "dot"}
-//                             onClick={() => setCurrentSlide(index)}
-//                         ></span>
-//                     ))}
-//                 </div>
-//             )}
-//         </div>
-//     );
-// }
 
